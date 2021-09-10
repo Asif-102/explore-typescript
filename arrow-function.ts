28cfcb8d786ea15a2b1c3f15a2bd334b040d2c58
Asif-102 <aiqbal517@gmail.com>
2021-09-11 02:27:36 +0600

    arrow function types

---
+++ arrow-function.ts
@@ -0,0 +1,6 @@
+const addNumbers = (num1: number, num2: number): number => num1 + num2;
+
+// void means this function has no return 
+const doMath = (x: number, y: number): void => {
+    console.log(x * y)
+}
---
+++ js/arrow-function.js
@@ -0,0 +1,6 @@
+"use strict";
+const addNumbers = (num1, num2) => num1 + num2;
+// void means this function has no return 
+const doMath = (x, y) => {
+    console.log(x * y);
+};
