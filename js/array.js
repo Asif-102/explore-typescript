61d2985edf6454850f6a02837368c3b47c98f1d0
Asif-102 <aiqbal517@gmail.com>
2021-09-11 02:12:47 +0600

    array types

---
+++ js/array.js
@@ -0,0 +1,13 @@
+"use strict";
+const salary = 7500;
+const friendSalaries = [7500, 12300, 17200, 9400, 8300];
+const friends = ['Sakib', 'Rakib', 'Nakib'];
+friendSalaries[0] = 10500;
+friendSalaries.push(13454);
+friends.push('Dakib');
+let max = 0;
+for (const salary of friendSalaries) {
+    if (salary > max) {
+        max = salary;
+    }
+}
