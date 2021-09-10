87155d147abb48c888bb555562243d28643ccdb7
Asif-102 <aiqbal517@gmail.com>
2021-09-11 02:39:59 +0600

    create interface for define class property and types

---
+++ js/interface.js
@@ -0,0 +1,21 @@
+"use strict";
+const messy = {
+    name: 'L Messy',
+    club: 'Abahoni',
+    salary: 12000,
+    wife: 'Nosimon ara begum'
+};
+const hannan = {
+    name: 'A. Hannan',
+    club: 'Kolabagan',
+    salary: 2000
+};
+const zuku = {
+    name: 'Zukerberg',
+    designation: 'Facebooker',
+    salary: 1100,
+    age: 41,
+    language: 'js',
+    codeEditor: 'VS Code',
+    typingSpeed: 65
+};
