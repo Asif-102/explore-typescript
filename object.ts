e143cf7617008e163d29d2146254a7711e6377bb
Asif-102 <aiqbal517@gmail.com>
2021-09-11 02:20:11 +0600

    object types

---
+++ object.ts
@@ -0,0 +1,20 @@
+const city: string = 'Dhaka';
+const fruits: string[] = ['mango', 'banana', 'apple'];
+
+type Person = {
+    name: string,
+    age: number,
+    salary: number
+}
+
+const student: Person = {
+    name: 'Kamal Uddin',
+    age: 32,
+    salary: 4500
+}
+
+const employee: { name: string, age: number, salary: number } = {
+    name: 'Solaiman',
+    age: 29,
+    salary: 1000000
+}
